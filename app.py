74c89a24dad33bbbee83fa85adb2fbfefcbccc04
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-08-23 13:46:20 +0900

    add procfile

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+cf_port = int(os.getenv("PORT"), 8000)
 
 @app.route('/')
 def index():
     return 'Hello world'
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=3456)
+    app.run(debug=False, host='0.0.0.0', port=cf_port)
