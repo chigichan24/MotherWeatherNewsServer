53b1cc5b46249f9af11213f37ef504f3c349e758
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-08-24 01:14:45 +0900

    add MotherState class which behaive with Singleton

--- api/sensors.py
+++ api/sensors.py
@@ -1,6 +1,9 @@
 from api.api import *
+from MotherState import *
 from EmotionEstimator import *
 import base64
+
+state = MotherStateSingleton()
 
 @app.route('/sensors', methods=['POST'])
 def receive_sensors():
@@ -9,11 +12,12 @@ def receive_sensors():
         raw_text = body.get("text")
         raw_image_data = body.get("image")
         if raw_text != None:
+            state.update()
             res = text_to_emotion(raw_text)
         if raw_image_data != None:
             d = raw_image_data.replace('//', '@').replace('@', '/')
             img_data = base64.b64decode(d)
             res = face_to_emotion(image = img_data)
-            print(res)
+            state.update()
         return jsonify({'error': None, 'data': "success"})
     return try_request(main)
--- api/weather.py
+++ api/weather.py
@@ -1,8 +1,9 @@
 from api.api import *
-
+from MotherState import *
+state = MotherStateSingleton()
 @app.route('/weather', methods=['GET'])
 def show_weather():
     def main():
-        weather = 0
-        return jsonify({'data': {'weather': weather}, 'error': None})
+        weather = state.weather
+        return jsonify({'data': {'weather': weather }, 'error': None})
     return try_request(main)
