c8be65fe293e6138ed2c3357465cba55d795f263
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-08-24 05:28:02 +0900

    trigger a warning, when the weather is thunder

--- MotherState.py
+++ MotherState.py
@@ -1,5 +1,5 @@
 import threading
-
+from NotificationTrigger import *
 class MotherStateSingleton:
     _instance = None
     _lock = threading.Lock()
@@ -17,3 +17,5 @@ class MotherStateSingleton:
     def update(self):
         self.weather += 1
         self.weather %= 4
+        if self.weather == 3:
+            trigger_to_firebase("気象庁", "母親雷警報発令中，母親の怒りに注意")
