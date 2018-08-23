94216d3230a437bf28ae4ed30535bb0abb3c472b
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-08-23 20:39:27 +0900

    enavle cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from api import api, sensors, weather
 import os
 
 app = Flask(__name__, static_url_path='')
+CORS(app)
 cf_port = int(os.getenv("PORT", 8080))
 
 app.register_blueprint(api.app)
